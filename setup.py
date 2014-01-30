d36277d42ffe7e46b6c8ee8fc0e1f6a567d8b4ab
cnlpepper <cnlpepper@4bf6d682-d48d-4364-819c-41f39e07019b>
2014-01-30 00:27:08 +0000

    Scenegraph _NodeBase and Node objects functionally complete, currently missing docstrings. Node test has been updated.

--- setup.py
+++ setup.py
@@ -9,6 +9,8 @@ extensions = [
     Extension("raysect.core.math.normal", ["raysect/core/math/normal.pyx"]),
     Extension("raysect.core.math.point", ["raysect/core/math/point.pyx"]),
     Extension("raysect.core.math.affinematrix", ["raysect/core/math/affinematrix.pyx"]),
+    Extension("raysect.core.scenegraph._nodebase",  ["raysect/core/scenegraph/_nodebase.pyx"]),
+    Extension("raysect.core.scenegraph.node",  ["raysect/core/scenegraph/node.pyx"]),
     Extension("raysect.tests.speed_test_functions", ["raysect/tests/speed_test_functions.pyx"]),
     ]
 
