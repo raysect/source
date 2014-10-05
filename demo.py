47bc5bec360cdf9691ff9067cd5a4f0a3f69add2
CnlPepper <cnlpepper@gmail.com>
2014-10-05 20:53:31 +0100

    Test commit following github migration.

--- demo.py
+++ demo.py
@@ -76,9 +76,9 @@ Box(Point(-50, -50, 50), Point(50, 50, 50.1), world, material=Checkerboard(5, d6
 
 
 camera = PinholeCamera(fov=45, parent=world, transform=translate(0, 0, -8) * rotate(0, 0, 0))
-camera.ray_max_depth = 20
-camera.rays = 1
-camera.spectral_samples = 25
+camera.ray_max_depth = 15
+camera.rays = 15
+camera.spectral_samples = 1
 camera.pixels = (256, 256)
 camera.display_progress = True
 camera.display_update_time = 10
