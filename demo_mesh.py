a740c40a07eeab58cb7adac73adb803a4209b3b0
CnlPepper <cnlpepper@gmail.com>
2015-06-28 18:45:33 +0100

    Updated demo for Matt.

--- demo_mesh.py
+++ demo_mesh.py
@@ -10,50 +10,57 @@ from matplotlib.pyplot import *
 from numpy import array
 from time import time
 
+BASE_PATH = "/home/alex/Shared/alex/work/MAST/mesh/MAST-M9"
+
 # Matt's STL test files, converted to obj (not public, we need to identify some CC files for demo purposes)
 mesh_list = [
-    "/home/alex/work/ccfe/MAST_stl_files/obj/MAST-M9-BEAM DUMPS + GDC.obj",
-    "/home/alex/work/ccfe/MAST_stl_files/obj/MAST-M9-CENTRE COLUMN ARMOUR.obj",
-    "/home/alex/work/ccfe/MAST_stl_files/obj/MAST-M9-LOWER COIL ARMOUR.obj",
-    "/home/alex/work/ccfe/MAST_stl_files/obj/MAST-M9-LOWER COILS.obj",
-    "/home/alex/work/ccfe/MAST_stl_files/obj/MAST-M9-LOWER ELM COILS.obj",
-    "/home/alex/work/ccfe/MAST_stl_files/obj/MAST-M9_P3_COILS_LOW_RES.obj",
-    "/home/alex/work/ccfe/MAST_stl_files/obj/MAST-M9-UPPER COIL ARMOUR.obj",
-    "/home/alex/work/ccfe/MAST_stl_files/obj/MAST-M9-UPPER COILS.obj",
-    "/home/alex/work/ccfe/MAST_stl_files/obj/MAST-M9-UPPER ELM COILS.obj",
-    # "/home/alex/work/ccfe/MAST_stl_files/obj/MAST-M9-MAST VESSEL.obj"
+    "MAST-M9-BEAM DUMPS + GDC",
+    "MAST-M9-CENTRE COLUMN ARMOUR",
+    "MAST-M9-LOWER COIL ARMOUR",
+    "MAST-M9-LOWER COILS",
+    "MAST-M9-LOWER ELM COILS",
+    "MAST-M9_P3_COILS_LOW_RES",
+    "MAST-M9-UPPER COIL ARMOUR",
+    "MAST-M9-UPPER COILS",
+    "MAST-M9-UPPER ELM COILS",
+    # "MAST-M9-MAST VESSEL"
 ]
 
 world = World()
 
 # obj convert to raysect optimised mesh
-# for filename in mesh_list:
+# for name in mesh_list:
+#
+#     filename = BASE_PATH + "/obj/" + name + ".obj"
 #     print("Reading {}...".format(filename))
 #     mesh = import_obj(
-#         filename, scaling=1e-3, kdtree_max_depth=16,
+#         filename, scaling=1e-3, kdtree_min_triangles=10,
 #         parent=world,
 #         transform=rotate(0, 90, 0),
 #         material=debug.Light(Vector(0.2, 0.0, 1.0), 0.4)
 #     )
 #
-#     print("Writing {}...".format(filename + ".rsom"))
-#     mesh.to_file(filename + ".rsom")
+#     filename = BASE_PATH + "/rsom/" + name + ".rsom"
+#     print("Writing {}...".format(filename))
+#     mesh.to_file(filename)
+#     print()
 
 # raysect mesh format (pickle)
-for filename in mesh_list:
-    print("Reading {}...".format(filename + ".rsom"))
+for name in mesh_list:
+    filename = BASE_PATH + "/rsom/" + name + ".rsom"
+    print("Reading {}...".format(filename))
     mesh = Mesh(
         parent=world,
         transform=rotate(0, 90, 0),
         material=debug.Light(Vector(0.2, 0.0, 1.0), 0.4)
-    ).from_file(filename + ".rsom")
+    ).from_file(filename)
 
 
 Box(Point(-50, -50, 50), Point(50, 50, 50.1), world, material=Checkerboard(4, d65_white, d65_white, 0.4, 0.8))
 Box(Point(-100, -100, -100), Point(100, 100, 100), world, material=UniformSurfaceEmitter(d65_white, 0.1))
 
 ion()
-camera = PinholeCamera(fov=60, parent=world, transform=translate(0, 0, -4) * rotate(0, 0, 0), process_count=4)
+camera = PinholeCamera(fov=60, parent=world, transform=translate(0, 0, -4) * rotate(0, 0, 0), process_count=3)
 camera.ray_max_depth = 15
 camera.rays = 1
 camera.spectral_samples = 15
