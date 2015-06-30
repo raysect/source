4a3744c948ec68edb2c982a08f2f046d1d27c365
CnlPepper <cnlpepper@gmail.com>
2015-07-01 00:35:47 +0100

    Work in progress, code is currently not functional.

--- raysect/core/math/kdtree.pyx
+++ raysect/core/math/kdtree.pyx
@@ -32,7 +32,7 @@
 from random import random
 from raysect.core.acceleration.boundingbox cimport BoundingBox
 from cpython.mem cimport PyMem_Malloc, PyMem_Realloc, PyMem_Free
-from libc.string cimport memcpy
+from libc.math cimport log, ceil
 cimport cython
 
 # this number of nodes will be pre-allocated when the kd-tree is initially created
@@ -44,13 +44,40 @@ DEF X_AXIS = 0  # branch, x-axis split
 DEF Y_AXIS = 1  # branch, y-axis split
 DEF Z_AXIS = 2  # branch, z-axis split
 
-# axis
 
+# c-structure that represent a kd-tree node
 cdef struct kdnode:
-    int type            # LEAF, BRANCH_X, BRANCH_Y, BRANCH_Z
+
+    int type        # LEAF, X_AXIS, Y_AXIS, Z_AXIS
     double split
-    int count           # upper index, item_count
+    int count       # upper index (BRANCH_*), item count (LEAF)
     int *items
+
+
+cdef class Item:
+    """
+    Item class. Represents an item to place into the kd-tree.
+
+    The id should be a unique integer value identifying an external object.
+    For example the id could be the index into an array of polygon objects.
+    The id values are stored in the kd-tree and returned by the hit() or
+    contains() methods.
+
+    A bounding box associated with the item defines the spatial extent of the
+    item along each axis. This data is used to place the items in the tree.
+
+    :param id: An integer item id.
+    :param box: A BoundingBox object defining the item's spatial extent.
+    """
+
+    cdef:
+        readonly int id
+        readonly BoundingBox box
+
+    def __init__(self, int id, BoundingBox box):
+
+        self.id = id
+        self.box = box
 
 
 cdef class KDTree:
@@ -59,6 +86,11 @@ cdef class KDTree:
         kdnode *_nodes
         int _allocated_nodes
         int _next_node
+        readonly BoundingBox bounds
+        int _max_depth
+        int _min_items
+        double _hit_cost
+        double _empty_bonus
 
     def __cinit__(self):
 
@@ -66,52 +98,163 @@ cdef class KDTree:
         self._allocated_nodes = 0
         self._next_node = 0
 
-    def __init__(self, items):
+    def __init__(self, items, max_depth=0, min_items=1, hit_cost=20.0, empty_bonus=0.2):
 
-        self.build(X_AXIS, items, None, 0)
-        # self.dump_info()
+        # sanity check
+        if empty_bonus < 0.0 or empty_bonus > 1.0:
+            raise ValueError("The empty_bonus cost modifier must lie in the range [0.0, 1.0].")
+        self._empty_bonus = empty_bonus
 
-    def dump_info(self):
+        # clamp other parameters
+        self._max_depth = max(0, max_depth)
+        self._min_items = max(1, min_items)
+        self._hit_cost = max(1.0, hit_cost)
+
+        # if max depth is set to zero, automatically calculate a reasonable depth
+        # tree depth is set to the value suggested in "Physically Based Rendering From Theory to
+        # Implementation 2nd Edition", Matt Phar and Greg Humphreys, Morgan Kaufmann 2010, p232
+        if self._max_depth == 0:
+            self._max_depth = <int> ceil(8 + 1.3 * log(len(items)))
+
+        # calculate kd-tree bounds
+        self.bounds = BoundingBox()
+        for item in items:
+            self.bounds.union(item.box)
+
+        # start build with the longest axis to try to avoid large narrow nodes
+        axis = self.bounds.largest_axis()
+        self.build(axis, items, self.bounds, 0)
+
+    def debug_print_all(self):
 
         for i in range(self._next_node):
             self.node_info(i)
 
-    def node_info(self, id):
+    def debug_print_node(self, id):
 
         if 0 <= id < self._next_node:
-
             if self._nodes[id].type == LEAF:
-
                 print("id={} LEAF: count {}, contents: [".format(id, self._nodes[id].count), end="")
                 for i in range(self._nodes[id].count):
                     print("{}".format(self._nodes[id].items[i]), end="")
                     if i < self._nodes[id].count - 1:
                         print(", ", end="")
                 print("]")
-
             else:
-
                 print("id={} BRANCH: axis {}, split {}, count {}".format(id, self._nodes[id].type, self._nodes[id].split, self._nodes[id].count))
 
+    # @cython.boundscheck(False)
+    # @cython.wraparound(False)
     cdef int build(self, int axis, list items, BoundingBox bounds, int depth):
 
-        # come in blind
-        # this function does a split test then creates appropriates node
+        # cdef:
+        #     int axis
+        #     tuple result
+        #     double split
+        #     list lower_triangle_data, upper_triangle_data
 
-        # it returns the id of it's node
+        if depth == 0 or len(items) <= self._min_items:
+            return self._new_leaf(items)
 
-        # DUMMY CODE
-        is_leaf = len(items) <= 4
-        lower_items = items[:len(items)/2]
-        lower_bounds = None
-        upper_items = items[len(items)/2:]
-        upper_bounds = None
-        split = <double> len(items) / 2
+        # attempt to identify a suitable node split
+        split_solution = self._split(axis, items, bounds)
 
-        if is_leaf:
+        # split solution found?
+        if split_solution is None:
             return self._new_leaf(items)
         else:
-            return self._new_branch(axis, split, lower_items, lower_bounds, upper_items, upper_bounds, depth)
+            return self._new_branch(axis, split_solution, depth)
+
+    # TODO: write _split
+    # TODO: write _hit
+    # TODO: write _contains
+
+
+    # @cython.boundscheck(False)
+    # @cython.wraparound(False)
+    # @cython.cdivision(True)
+    # cdef tuple _split(self, int axis, list triangle_data, BoundingBox node_bounds, double hit_cost):
+    #
+    #     cdef:
+    #         double split, cost, best_cost, best_split
+    #         bint is_leaf
+    #         int lower_triangle_count, upper_triangle_count
+    #         double recip_total_sa, lower_sa, upper_sa
+    #         list edges, lower_triangle_data, upper_triangle_data
+    #         _Edge edge
+    #         _TriangleData data
+    #
+    #     # store cost of leaf as current best solution
+    #     best_cost = len(triangle_data) * hit_cost
+    #     best_split = 0
+    #     is_leaf = True
+    #
+    #     # cache reciprocal of node's surface area
+    #     recip_total_sa = 1.0 / node_bounds.surface_area()
+    #
+    #     # obtain sorted list of candidate edges along chosen axis
+    #     edges = self._build_edges(triangle_data, axis)
+    #
+    #     # cache triangle counts in lower and upper volumes for speed
+    #     lower_triangle_count = 0
+    #     upper_triangle_count = len(triangle_data)
+    #
+    #     # scan through candidate edges from lowest to highest
+    #     for edge in edges:
+    #
+    #         # update primitive counts for upper volume
+    #         # note: this occasionally creates invalid solutions if edges of
+    #         # boxes are coincident however the invalid solutions cost
+    #         # more than the valid solutions and will not be selected
+    #         if edge.is_upper_edge:
+    #             upper_triangle_count -= 1
+    #
+    #         # a split on the node boundary serves no useful purpose
+    #         # only consider edges that lie inside the node bounds
+    #         split = edge.value
+    #         if node_bounds.lower.get_index(axis) < split < node_bounds.upper.get_index(axis):
+    #
+    #             # calculate surface area of split volumes
+    #             lower_sa = self._calc_lower_bounds(node_bounds, split, axis).surface_area()
+    #             upper_sa = self._calc_upper_bounds(node_bounds, split, axis).surface_area()
+    #
+    #             # calculate SAH cost
+    #             cost = 1 + (lower_sa * lower_triangle_count + upper_sa * upper_triangle_count) * recip_total_sa * hit_cost
+    #
+    #             # has a better split been found?
+    #             if cost < best_cost:
+    #                 best_cost = cost
+    #                 best_split = split
+    #                 is_leaf = False
+    #
+    #         # update triangle counts for lower volume
+    #         # note: this occasionally creates invalid solutions if edges of
+    #         # boxes are coincident however the invalid solutions cost
+    #         # more than the valid solutions and will not be selected
+    #         if not edge.is_upper_edge:
+    #             lower_triangle_count += 1
+    #
+    #     if is_leaf:
+    #         return None
+    #
+    #     # using cached values split triangles into two lists
+    #     # note the split boundary is defined as lying in the upper node
+    #     lower_triangle_data = []
+    #     upper_triangle_data = []
+    #     for data in triangle_data:
+    #
+    #         # is the triangle present in the lower node?
+    #         if data.lower_extent[axis] < best_split:
+    #             lower_triangle_data.append(data)
+    #
+    #         # is the triangle present in the upper node?
+    #         if data.upper_extent[axis] >= best_split:
+    #             upper_triangle_data.append(data)
+    #
+    #
+    #   TODO: add bounds calculations here
+    #
+    #     return best_split, lower_triangle_data, upper_triangle_data
 
     cdef int _new_node(self):
 
@@ -155,19 +298,29 @@ cdef class KDTree:
 
         return id
 
-    cdef int _new_branch(self, int axis, double split, list lower_items, BoundingBox lower_bounds, list upper_items, BoundingBox upper_bounds, int depth):
+    cdef int _new_branch(self, int axis, tuple split_solution, int depth):
 
         cdef:
             int id, upper_id
+            int next_axis
+            double split
+            list lower_items, upper_items
+            BoundingBox lower_bounds,  upper_bounds
 
         id = self._new_node()
+
+        # cycle to the next axis
+        next_axis = (axis + 1) % 3
+
+        # unpack split solution
+        split, lower_items, lower_bounds, upper_items, upper_bounds = split_solution
 
         # recursively build lower and upper nodes
         # the lower node is always the next node in the list
         # the upper node may be an arbitrary distance along the list
         # we store the upper node id in count for future evaluation
-        self.build(axis, lower_items, lower_bounds, depth + 1)
-        upper_id = self.build(axis, upper_items, upper_bounds, depth + 1)
+        self.build(next_axis, lower_items, lower_bounds, depth + 1)
+        upper_id = self.build(next_axis, upper_items, upper_bounds, depth + 1)
 
         # WARNING: Don't "optimise" this code by writing self._nodes[id].count = self._build(...)
         # it appears that the self._nodes[id] is de-referenced *before* the call to _build() and
