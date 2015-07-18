3c020ca1f17bcf8163bda713719a826cb205ae81
cnlpepper <cnlpepper@gmail.com>
2015-07-18 22:59:13 +0100

    Updated demo for Matt's interest!

--- demo_mesh.py
+++ demo_mesh.py
@@ -15,16 +15,16 @@ BASE_PATH = "/home/alex/Shared/alex/work/MAST/mesh/MAST-M9"
 
 # Matt's STL test files, converted to obj (not public, we need to identify some CC files for demo purposes)
 mesh_list = [
-    "MAST-M9-BEAM DUMPS + GDC",
-    "MAST-M9-CENTRE COLUMN ARMOUR",
-    "MAST-M9-LOWER COIL ARMOUR",
-    "MAST-M9-LOWER COILS",
-    "MAST-M9-LOWER ELM COILS",
-    "MAST-M9_P3_COILS_LOW_RES",
-    "MAST-M9-UPPER COIL ARMOUR",
-    "MAST-M9-UPPER COILS",
-    "MAST-M9-UPPER ELM COILS",
-    # "MAST-M9-MAST VESSEL"
+    # "MAST-M9-BEAM DUMPS + GDC",
+     "MAST-M9-CENTRE COLUMN ARMOUR",
+    # "MAST-M9-LOWER COIL ARMOUR",
+    # "MAST-M9-LOWER COILS",
+    # "MAST-M9-LOWER ELM COILS",
+    # "MAST-M9_P3_COILS_LOW_RES",
+    # "MAST-M9-UPPER COIL ARMOUR",
+    # "MAST-M9-UPPER COILS",
+    # "MAST-M9-UPPER ELM COILS",
+    # MAST-M9-MAST VESSEL"
 ]
 
 world = World()
@@ -58,6 +58,19 @@ for name in mesh_list:
     )
     mesh.load(filename)
 
+
+for i in range(5):
+    for j in range(5):
+        if i != 2 or j != 2:
+            Mesh(instance=mesh,
+                 parent=world,
+                 transform=translate((i-2)*6, (j-2)*6, 0)*rotate(0, 90, 0),
+                 material=debug.Light(Vector(0.2, 0.0, 1.0), 0.4)
+            )
+
+mesh.load(BASE_PATH + "/rsom/MAST-M9_P3_COILS_LOW_RES.rsom")
+
+
 print("Rendering...")
 # Subtract(mesh, Box(Point(-5, -5, -5), Point(5, 5, 0)), material=debug.Light(Vector(0.2, 0.0, 1.0), 0.4), parent=world)
 
@@ -65,11 +78,11 @@ Box(Point(-50, -50, 50), Point(50, 50, 50.1), world, material=Checkerboard(4, d6
 Box(Point(-100, -100, -100), Point(100, 100, 100), world, material=UniformSurfaceEmitter(d65_white, 0.1))
 
 ion()
-camera = PinholeCamera(fov=60, parent=world, transform=translate(0, 0, -4) * rotate(0, 0, 0), process_count=4)
+camera = PinholeCamera(fov=60, parent=world, transform=translate(0, 0, -25) * rotate(0, 0, 0), process_count=4)
 camera.ray_max_depth = 15
 camera.rays = 1
 camera.spectral_samples = 15
-camera.pixels = (512, 512)
+camera.pixels = (128, 128)
 camera.display_progress = True
 camera.display_update_time = 5
 camera.super_samples = 3
