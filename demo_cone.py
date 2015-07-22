959a319594544116d8de5b223d371a977a20804f
cnlpepper <cnlpepper@gmail.com>
2015-07-22 19:14:46 +0100

    Identified a failing case for Matt.

--- demo_cone.py
+++ demo_cone.py
@@ -7,15 +7,16 @@ from raysect.optical.material.glass_libraries import schott
 from raysect.primitive import Sphere, Box, Cylinder, Union, Intersect, Subtract, Cone
 from matplotlib.pyplot import *
 from numpy import array
+from raysect.optical.material.debug import Light
 
 import sys
 sys.ps1 = 'SOMETHING'
 
 world = World()
 
-cone = Cone(material=schott("N-BK7"), parent=world)
+cone = Cone(material=Light(Vector(0.2, -0.1, 1.0)), parent=world, transform=rotate(0, 150, 0))
 
-cyl_x = Cylinder(1, 4.2, transform=rotate(90, 0, 0)*translate(0, 0, -2.1), material=schott("N-BK7"), parent=world)
+# cyl_x = Cylinder(1, 4.2, transform=rotate(90, 0, 0)*translate(0, 0, -2.1), material=schott("N-BK7"), parent=world)
 
 Box(Point(-50, -50, 50), Point(50, 50, 50.1), world, material=Checkerboard(4, d65_white, d65_white, 0.4, 0.8))
 Box(Point(-100, -100, -100), Point(100, 100, 100), world, material=UniformSurfaceEmitter(d65_white, 0.1))
@@ -23,9 +24,9 @@ Box(Point(-100, -100, -100), Point(100, 100, 100), world, material=UniformSurfac
 ion()
 camera = PinholeCamera(fov=45, parent=world, transform=translate(0, 0, -4) * rotate(0, 0, 0))
 camera.ray_max_depth = 10
-camera.rays = 10
-camera.spectral_samples = 1
-camera.pixels = (1024, 1024)
+camera.rays = 1
+camera.spectral_samples = 21
+camera.pixels = (256, 256)
 camera.display_progress = True
 camera.display_update_time = 10
 camera.observe()
