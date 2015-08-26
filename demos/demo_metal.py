d27251f28c018cb7e2433552df7994bc3caaceba
CnlPepper <cnlpepper@gmail.com>
2015-08-26 23:35:00 +0100

    Added more metals, Added a demo. Expanded material module import.

---
+++ demos/demo_metal.py
@@ -0,0 +1,47 @@
+from raysect.optical import World, Node, translate, rotate, Point, d65_white, ConstantSF
+from raysect.primitive import Sphere, Box, Cylinder
+from raysect.optical.observer import PinholeCamera
+from raysect.optical.material import Conductor, Lambert, UniformSurfaceEmitter
+from raysect.optical.material import gold_n, gold_k
+from raysect.optical.material import silver_n, silver_k
+from raysect.optical.material import copper_n, copper_k
+from raysect.optical.material import titanium_n, titanium_k
+from raysect.optical.material import aluminium_n, aluminium_k
+from matplotlib.pyplot import *
+
+world = World()
+
+Sphere(0.5, world, transform=translate(1.2, 0.5001, 0.6), material=Conductor(gold_n, gold_k))
+Sphere(0.5, world, transform=translate(0.6, 0.5001, -0.6), material=Conductor(silver_n, silver_k))
+Sphere(0.5, world, transform=translate(0, 0.5001, 0.6), material=Conductor(copper_n, copper_k))
+Sphere(0.5, world, transform=translate(-0.6, 0.5001, -0.6), material=Conductor(titanium_n, titanium_k))
+Sphere(0.5, world, transform=translate(-1.2, 0.5001, 0.6), material=Conductor(aluminium_n, aluminium_k))
+
+Box(Point(-100, -0.1, -100), Point(100, 0, 100), world, material=Lambert(ConstantSF(0.5)))
+Cylinder(3.0, 8.0, world, transform=translate(4, 8, 0) * rotate(90, 0, 0), material=UniformSurfaceEmitter(d65_white, 1.0))
+
+camera = PinholeCamera(parent=world, transform=translate(0, 4, -3.5) * rotate(0, -48, 0))
+camera.ray_min_depth = 3
+camera.ray_max_depth = 500
+camera.ray_extinction_prob = 0.01
+camera.rays = 1
+camera.spectral_samples = 15
+camera.pixels = (512, 512)
+camera.pixel_samples = 25
+camera.sub_sample = True
+camera.display_progress = True
+camera.display_update_time = 10
+camera.accumulate = True
+
+# start ray tracing
+ion()
+for p in range(1, 1000):
+    print("Rendering pass {} ({} samples/pixel)...".format(p, camera.accumulated_samples + camera.pixel_samples * camera.rays))
+    camera.observe()
+    camera.save("demo_metal_{}_samples.png".format(camera.accumulated_samples))
+    print()
+
+# display final result
+ioff()
+camera.display()
+show()
