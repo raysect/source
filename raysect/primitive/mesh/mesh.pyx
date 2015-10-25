ae7c5b61bc1391ffba3895870eee7acdf3874dc2
CnlPepper <cnlpepper@gmail.com>
2015-10-25 16:43:40 +0000

    Work in progress.

--- raysect/primitive/mesh/mesh.pyx
+++ raysect/primitive/mesh/mesh.pyx
@@ -38,6 +38,8 @@ from raysect.core.math.kdtree cimport KDTreeCore, Item
 from raysect.core.classes cimport Material, Intersection, Ray, new_intersection, new_ray
 from raysect.core.acceleration.boundingbox cimport BoundingBox, new_boundingbox
 from libc.math cimport fabs, log, ceil
+from numpy import array, float32, int64, zeros
+from numpy cimport ndarray
 import io
 import pickle
 cimport cython
@@ -61,50 +63,229 @@ higher precision to resolve cases when the edge tests result in a degenerate sol
 extremely small triangles that are being tested against a ray with an origin far from the mesh.
 """
 
-cdef class Triangle:
+# cdef class Triangle:
+#
+#     cdef:
+#         readonly Point v1, v2, v3
+#         readonly Normal n1, n2, n3
+#         readonly Normal face_normal
+#         bint _smoothing_enabled
+#
+#     def __init__(self, Point v1 not None, Point v2 not None, Point v3 not None,
+#                  Normal n1=None, Normal n2=None, Normal n3=None):
+#
+#         self.v1 = v1
+#         self.v2 = v2
+#         self.v3 = v3
+#         self._calc_face_normal()
+#
+#         # if any of the vertex normals is missing, disable interpolation
+#         if n1 is None or n2 is None or n3 is None:
+#
+#             self._smoothing_enabled = False
+#             self.n1 = None
+#             self.n2 = None
+#             self.n3 = None
+#
+#         else:
+#
+#             self._smoothing_enabled = True
+#             self.n1 = n1.normalise()
+#             self.n2 = n2.normalise()
+#             self.n3 = n3.normalise()
+#
+#     def __getstate__(self):
+#         """Encodes state for pickling."""
+#
+#         return self.v1, self.v2, self.v3, self.n1, self.n2, self.n3, self.face_normal, self._smoothing_enabled
+#
+#     def __setstate__(self, state):
+#         """Decodes state for pickling."""
+#
+#         self.v1, self.v2, self.v3, self.n1, self.n2, self.n3, self.face_normal, self._smoothing_enabled = state
+#
+#     cdef Normal _calc_face_normal(self):
+#         """
+#         Calculate the triangles face normal from the vertices.
+#
+#         The triangle face normal direction is defined by the right hand screw
+#         rule. When looking at the triangle from the back face, the vertices
+#         will be ordered in a clockwise fashion and the normal will be pointing
+#         away from the observer.
+#         """
+#
+#         cdef:
+#             Vector a, b, c
+#
+#         a = self.v1.vector_to(self.v2)
+#         b = self.v1.vector_to(self.v3)
+#         c = a.cross(b).normalise()
+#         self.face_normal = new_normal(c.x, c.y, c.z)
+#
+#     @cython.cdivision(True)
+#     cpdef Point centre_point(self):
+#         """
+#         Returns the centre point (centroid) of the triangle.
+#         :return: Point object.
+#         """
+#
+#         return new_point(
+#             (self.v1.x + self.v2.x + self.n3.x) / 3,
+#             (self.v1.y + self.v2.y + self.n3.y) / 3,
+#             (self.v1.z + self.v2.z + self.n3.z) / 3
+#         )
+#
+#     cpdef Normal interpolate_normal(self, double u, double v, double w, bint smoothing=True):
+#         """
+#         Returns the surface normal for the specified barycentric coordinate.
+#
+#         The result is undefined if u, v or w are outside the range [0, 1].
+#         If smoothing is disabled the result will be the face normal.
+#
+#         :param u: Barycentric U coordinate.
+#         :param v: Barycentric V coordinate.
+#         :param w: Barycentric W coordinate.
+#         :return: The surface normal at the specified coordinate.
+#         """
+#
+#         if smoothing and self._smoothing_enabled:
+#             return new_normal(
+#                 u * self.n1.x + v * self.n2.x + w * self.n3.x,
+#                 u * self.n1.y + v * self.n2.y + w * self.n3.y,
+#                 u * self.n1.z + v * self.n2.z + w * self.n3.z,
+#             )
+#         else:
+#             return self.face_normal
+#
+#     cpdef double lower_extent(self, int axis):
+#         """
+#         Returns the lowest extent of the triangle along the specified axis.
+#         """
+#
+#         return min(self.v1.get_index(axis),
+#                    self.v2.get_index(axis),
+#                    self.v3.get_index(axis))
+#
+#     cpdef double upper_extent(self, int axis):
+#         """
+#         Returns the upper extent of the triangle along the specified axis.
+#         """
+#
+#         return max(self.v1.get_index(axis),
+#                    self.v2.get_index(axis),
+#                    self.v3.get_index(axis))
+#
+#     cpdef BoundingBox bounding_box(self):
+#         """
+#         Returns a bounding box enclosing the triangle.
+#
+#         The box is defined in the triangle's coordinate system. A small degree
+#         of padding is added to the bounding box to provide the conservative
+#         bounds required by the watertight mesh algorithm.
+#
+#         :return: A BoundingBox object.
+#         """
+#
+#         bbox = new_boundingbox(
+#             Point(
+#                 min(self.v1.x, self.v2.x, self.v3.x),
+#                 min(self.v1.y, self.v2.y, self.v3.y),
+#                 min(self.v1.z, self.v2.z, self.v3.z),
+#             ),
+#             Point(
+#                 max(self.v1.x, self.v2.x, self.v3.x),
+#                 max(self.v1.y, self.v2.y, self.v3.y),
+#                 max(self.v1.z, self.v2.z, self.v3.z),
+#             ),
+#         )
+#         bbox.pad(bbox.largest_extent() * BOX_PADDING)
+#         return bbox
+#
+
+"""
+ok the plan:
+
+mesh will take 3 arrays:
+* vertices, normals, triangles
+checks are performed to validate data, as bounds checking will be disabled internally
+face normals are calculated and cached
+items are assembled
+init done!
+
+everything needs converting to float32
+
+
+
+
+
+"""
+
+
+cdef class MeshKDTree(KDTreeCore):
 
     cdef:
-        readonly Point v1, v2, v3
-        readonly Normal n1, n2, n3
-        readonly Normal face_normal
-        bint _smoothing_enabled
+        ndarray vertices
+        ndarray vertex_normals
+        ndarray face_normals
+        ndarray triangles
 
-    def __init__(self, Point v1 not None, Point v2 not None, Point v3 not None,
-                 Normal n1=None, Normal n2=None, Normal n3=None):
+        tuple _hit_ray_transform
+        readonly tuple hit_intersection
 
-        self.v1 = v1
-        self.v2 = v2
-        self.v3 = v3
-        self._calc_face_normal()
+    def __init__(self, object vertices, object normals, object triangles, int max_depth=0, int min_items=1, double hit_cost=20.0, double empty_bonus=0.2):
 
-        # if any of the vertex normals is missing, disable interpolation
-        if n1 is None or n2 is None or n3 is None:
+        self.vertices = array(vertices, dtype=float32)
+        self.triangles = array(triangles, dtype=int64)
+        if normals is not None:
+            self.vertex_normals = array(normals, dtype=float32)
 
-            self._smoothing_enabled = False
-            self.n1 = None
-            self.n2 = None
-            self.n3 = None
+        # check dimensions are correct
+        if len(vertices.shape) != 2 or vertices.shape[1] != 3:
+            raise ValueError("The vertex array must have dimensions Nx3.")
+
+        if normals is not None:
+
+            if len(normals.shape) != 2 or normals.shape[1] != 3:
+                raise ValueError("The normal array must have dimensions Nx3.")
+
+            if len(triangles.shape) != 2 or triangles.shape[1] != 6:
+                raise ValueError("The triangle array must have dimensions Nx6.")
 
         else:
 
-            self._smoothing_enabled = True
-            self.n1 = n1.normalise()
-            self.n2 = n2.normalise()
-            self.n3 = n3.normalise()
+            if len(triangles.shape) != 2 or triangles.shape[1] != 3:
+                raise ValueError("The triangle array must have dimensions Nx3.")
 
-    def __getstate__(self):
-        """Encodes state for pickling."""
+        # check triangles contains only valid indices
+        invalid = (self.triangles[:, 0:3] < 0) | (self.triangles[:, 0:3] > self.vertices.shape[1])
+        if invalid.any():
+            raise ValueError("The triangle array references non-existent vertices.")
 
-        return self.v1, self.v2, self.v3, self.n1, self.n2, self.n3, self.face_normal, self._smoothing_enabled
+        if normals is not None:
+            invalid = (self.triangles[:, 3:6] < 0) | (self.triangles[:, 3:6] > self.vertex_normals.shape[1])
+            if invalid.any():
+                raise ValueError("The triangle array references non-existent normals.")
 
-    def __setstate__(self, state):
-        """Decodes state for pickling."""
+        # generate face normals
+        self._calc_face_normals()
 
-        self.v1, self.v2, self.v3, self.n1, self.n2, self.n3, self.face_normal, self._smoothing_enabled = state
 
-    cdef Normal _calc_face_normal(self):
+
+
+
+        self._hit_ray_transform = None
+        self.hit_intersection = None
+
+        # # kd-Tree init requires the triangle's id (it's index here) and bounding box
+        # items = []
+        # for id, triangle in enumerate(triangles):
+        #     items.append(Item(id, triangle.bounding_box()))
+        #
+        # super().__init__(items, max_depth, min_items, hit_cost, empty_bonus)
+
+    cdef void _calc_face_normals(self):
         """
-        Calculate the triangles face normal from the vertices.
+        Calculate the triangles face normals from the vertices.
 
         The triangle face normal direction is defined by the right hand screw
         rule. When looking at the triangle from the back face, the vertices
@@ -113,167 +294,81 @@ cdef class Triangle:
         """
 
         cdef:
-            Vector a, b, c
+            int i1, i2, i3
+            Point p1, p2, p3
+            Vector v1, v2, v3
 
-        a = self.v1.vector_to(self.v2)
-        b = self.v1.vector_to(self.v3)
-        c = a.cross(b).normalise()
-        self.face_normal = new_normal(c.x, c.y, c.z)
+        self.face_normals = zeros((self.triangles.shape[0], 3))
+        for i in self.face_normals.shape[0]:
 
-    @cython.cdivision(True)
-    cpdef Point centre_point(self):
-        """
-        Returns the centre point (centroid) of the triangle.
-        :return: Point object.
-        """
+            i1 = self.triangles[i, 0]
+            i2 = self.triangles[i, 1]
+            i3 = self.triangles[i, 2]
 
-        return new_point(
-            (self.v1.x + self.v2.x + self.n3.x) / 3,
-            (self.v1.y + self.v2.y + self.n3.y) / 3,
-            (self.v1.z + self.v2.z + self.n3.z) / 3
-        )
+            p1 = Point(*self.vertices[i1, :])
+            p2 = Point(*self.vertices[i2, :])
+            p3 = Point(*self.vertices[i3, :])
 
-    cpdef Normal interpolate_normal(self, double u, double v, double w, bint smoothing=True):
-        """
-        Returns the surface normal for the specified barycentric coordinate.
+            v1 = p1.vector_to(p2)
+            v2 = p1.vector_to(p3)
+            v3 = v1.cross(v2).normalise()
 
-        The result is undefined if u, v or w are outside the range [0, 1].
-        If smoothing is disabled the result will be the face normal.
-
-        :param u: Barycentric U coordinate.
-        :param v: Barycentric V coordinate.
-        :param w: Barycentric W coordinate.
-        :return: The surface normal at the specified coordinate.
-        """
-
-        if smoothing and self._smoothing_enabled:
-            return new_normal(
-                u * self.n1.x + v * self.n2.x + w * self.n3.x,
-                u * self.n1.y + v * self.n2.y + w * self.n3.y,
-                u * self.n1.z + v * self.n2.z + w * self.n3.z,
-            )
-        else:
-            return self.face_normal
-
-    cpdef double lower_extent(self, int axis):
-        """
-        Returns the lowest extent of the triangle along the specified axis.
-        """
-
-        return min(self.v1.get_index(axis),
-                   self.v2.get_index(axis),
-                   self.v3.get_index(axis))
-
-    cpdef double upper_extent(self, int axis):
-        """
-        Returns the upper extent of the triangle along the specified axis.
-        """
-
-        return max(self.v1.get_index(axis),
-                   self.v2.get_index(axis),
-                   self.v3.get_index(axis))
-
-    cpdef BoundingBox bounding_box(self):
-        """
-        Returns a bounding box enclosing the triangle.
-
-        The box is defined in the triangle's coordinate system. A small degree
-        of padding is added to the bounding box to provide the conservative
-        bounds required by the watertight mesh algorithm.
-
-        :return: A BoundingBox object.
-        """
-
-        bbox = new_boundingbox(
-            Point(
-                min(self.v1.x, self.v2.x, self.v3.x),
-                min(self.v1.y, self.v2.y, self.v3.y),
-                min(self.v1.z, self.v2.z, self.v3.z),
-            ),
-            Point(
-                max(self.v1.x, self.v2.x, self.v3.x),
-                max(self.v1.y, self.v2.y, self.v3.y),
-                max(self.v1.z, self.v2.z, self.v3.z),
-            ),
-        )
-        bbox.pad(bbox.largest_extent() * BOX_PADDING)
-        return bbox
-
-
-cdef class MeshKDTree(KDTreeCore):
-
-    cdef:
-        list triangles
-        tuple _hit_ray_transform
-        readonly tuple hit_intersection
-
-    def __init__(self, list triangles, int max_depth=0, int min_items=1, double hit_cost=20.0, double empty_bonus=0.2):
-
-        self.triangles = triangles
-        self._hit_ray_transform = None
-        self.hit_intersection = None
-
-        # kd-Tree init requires the triangle's id (it's index here) and bounding box
-        items = []
-        for id, triangle in enumerate(triangles):
-            items.append(Item(id, triangle.bounding_box()))
-
-        super().__init__(items, max_depth, min_items, hit_cost, empty_bonus)
+            self.face_normals[i, 0:3] = v3.x, v3.y, v3.z
 
     def __getstate__(self):
         """Encodes state for pickling."""
 
-        return self.triangles, super().__getstate__()
+        # return self.triangles, super().__getstate__()
 
     def __setstate__(self, state):
         """Decodes state for pickling."""
 
-        self.triangles, base_state = state
-        super().__setstate__(base_state)
+        # self.triangles, base_state = state
+        # super().__setstate__(base_state)
 
     cpdef bint hit(self, Ray ray):
 
-        self.hit_intersection = None
-        self._calc_rayspace_transform(ray)
-        return self._hit(ray)
+        # self.hit_intersection = None
+        # self._calc_rayspace_transform(ray)
+        # return self._hit(ray)
 
     @cython.boundscheck(False)
     @cython.wraparound(False)
     cdef bint _hit_leaf(self, int id, Ray ray, double max_range):
 
-        cdef:
-            int count, item, index
-            double distance
-            double t, u, v, w
-            Triangle triangle, closest_triangle
-            tuple intersection, closest_intersection
-
-        # unpack leaf data
-        count = self._nodes[id].count
-
-        # find the closest triangle-ray intersection with initial search distance limited by node and ray limits
-        distance = min(ray.max_distance, max_range)
-        closest_intersection = None
-        closest_triangle = None
-        for item in range(count):
-
-            # dereference the triangle
-            index = self._nodes[id].items[item]
-            triangle = self.triangles[index]
-
-            # test for intersection
-            intersection = self._hit_triangle(triangle, ray)
-            if intersection is not None and intersection[0] < distance:
-                distance = intersection[0]
-                closest_triangle = triangle
-                closest_intersection = intersection
-
-        if closest_intersection is None:
-            return False
-
-        t, u, v, w = closest_intersection
-        self.hit_intersection = closest_triangle, t, u, v, w
-        return True
+        # cdef:
+        #     int count, item, index
+        #     double distance
+        #     double t, u, v, w
+        #     Triangle triangle, closest_triangle
+        #     tuple intersection, closest_intersection
+        #
+        # # unpack leaf data
+        # count = self._nodes[id].count
+        #
+        # # find the closest triangle-ray intersection with initial search distance limited by node and ray limits
+        # distance = min(ray.max_distance, max_range)
+        # closest_intersection = None
+        # closest_triangle = None
+        # for item in range(count):
+        #
+        #     # dereference the triangle
+        #     index = self._nodes[id].items[item]
+        #     triangle = self.triangles[index]
+        #
+        #     # test for intersection
+        #     intersection = self._hit_triangle(triangle, ray)
+        #     if intersection is not None and intersection[0] < distance:
+        #         distance = intersection[0]
+        #         closest_triangle = triangle
+        #         closest_intersection = intersection
+        #
+        # if closest_intersection is None:
+        #     return False
+        #
+        # t, u, v, w = closest_intersection
+        # self.hit_intersection = closest_triangle, t, u, v, w
+        # return True
 
     @cython.cdivision(True)
     cdef void _calc_rayspace_transform(self, Ray ray):
@@ -317,7 +412,7 @@ cdef class MeshKDTree(KDTreeCore):
         self._hit_ray_transform = ix, iy, iz, sx, sy, sz
 
     @cython.cdivision(True)
-    cdef tuple _hit_triangle(self, Triangle triangle, Ray ray):
+    cdef tuple _hit_triangle(self, int triangle, Ray ray):
 
         # This code is a Python port of the code listed in appendix A of
         #  "Watertight Ray/Triangle Intersection", S.Woop, C.Benthin, I.Wald,
@@ -334,6 +429,8 @@ cdef class MeshKDTree(KDTreeCore):
 
         # unpack ray transform
         ix, iy, iz, sx, sy, sz = self._hit_ray_transform
+
+        # todo: can get rid of point use here: use float[3] instead
 
         # center coordinate space on ray origin
         v1 = new_point(triangle.v1.x - ray.origin.x, triangle.v1.y - ray.origin.y, triangle.v1.z - ray.origin.z)
