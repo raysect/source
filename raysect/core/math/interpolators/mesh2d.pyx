47889fbd7d4ac31139e18e3293c904a76f1046cf
cnlpepper <cnlpepper@gmail.com>
2015-12-30 02:10:11 +0000

    Added todo items for future refactor.

--- raysect/core/math/interpolators/mesh2d.pyx
+++ raysect/core/math/interpolators/mesh2d.pyx
@@ -46,6 +46,8 @@ DEF V3 = 2
 DEF X = 0
 DEF Y = 1
 
+# TODO: split instance into its own method (e.g. mymesh.instance(vertex_data=...)), current init interface is messy
+
 
 cdef class Interpolator2DMesh(Function2D):
     """
