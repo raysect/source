da2413b4ab93f0bfe21346e13b4adb73c7660265
matt <matthewncarr@gmail.com>
2017-02-19 17:07:37 +0000

    Minor corrections to core.math inits.

--- raysect/core/math/__init__.py
+++ raysect/core/math/__init__.py
@@ -30,7 +30,7 @@
 # POSSIBILITY OF SUCH DAMAGE.
 
 from .point import Point2D, Point3D
-from .vector import Vector3D
+from .vector import Vector2D, Vector3D
 from .normal import Normal3D
 from .affinematrix import AffineMatrix3D
 from .transform import translate, rotate_x, rotate_y, rotate_z, rotate_vector, rotate, rotate_basis
