f1439a29407494c5eb77702941ae5412a17bd172
matt <matthewncarr@gmail.com>
2017-02-26 16:41:58 +0000

    Another test, trying to use web versions of css files with extra empty stylesheet.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -137,12 +137,14 @@ html_favicon = "RaysectLogo_web.ico"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['static']
-#
-# html_context = {'css_files': [
-#     '_static/theme_overrides.css',  # override wide tables in RTD theme
-#     ]
-# }
+html_static_path = ['static']
+
+html_context = {'css_files': [
+    'https://media.readthedocs.org/css/sphinx_rtd_theme.css',
+    'https://media.readthedocs.org/css/readthedocs-doc-embed.css'
+    '_static/theme_overrides.css',  # override wide tables in RTD theme
+    ]
+}
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
