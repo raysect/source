a5f0ab3ae74cfee0e33ff8c283392b9187c06e12
Matthew Carr <MatthewNCarr@gmail.com>
2017-10-16 20:01:40 +0100

    Work in progress on the targeted pixel observer. Towards #178.

---
+++ raysect/optical/observer/nonimaging/targeted_pixel.pyx
@@ -0,0 +1,221 @@
+# cython: language_level=3
+
+# Copyright (c) 2014-2017, Dr Alex Meakins, Raysect Project
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#     1. Redistributions of source code must retain the above copyright notice,
+#        this list of conditions and the following disclaimer.
+#
+#     2. Redistributions in binary form must reproduce the above copyright
+#        notice, this list of conditions and the following disclaimer in the
+#        documentation and/or other materials provided with the distribution.
+#
+#     3. Neither the name of the Raysect Project nor the names of its
+#        contributors may be used to endorse or promote products derived from
+#        this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+from libc.math cimport cos, M_PI as PI
+
+from raysect.core.math.sampler cimport RectangleSampler3D, HemisphereCosineSampler
+from raysect.core.boundingsphere cimport BoundingSphere3D
+from raysect.optical cimport Primitive, Ray, Point3D, new_point3d, new_vector3d
+from raysect.optical.observer.base cimport Observer0D
+from raysect.optical.observer.pipeline.spectral import SpectralPipeline0D
+cimport cython
+
+
+# TODO - make sure the cached bounding sphere is reset when a change to the scenegraph occurs
+cdef class TargetedPixel(Observer0D):
+    """
+    A pixel observer that samples are targeted solid angle and rectangular area.
+
+    If the user wants to target and empty space or a hole in a structure. They should
+    create a primitive that occupies the space and give it a Null material.
+
+    .. Warning..
+       This should only be used when the only source of light lies in the solid angle
+       of the targeted primitive as observed by the observer. If this is not the case any
+       power calculations will be invalid.
+
+    :param Primitive target: The primitive that this observer will target.
+    :param list pipelines: The list of pipelines that will process the spectrum measured
+      by this pixel (default=SpectralPipeline0D()).
+    :param float x_width: The rectangular collection area's width along the
+      x-axis in local coordinates (default=1cm).
+    :param float y_width: The rectangular collection area's width along the
+      y-axis in local coordinates (default=1cm).
+    :param kwargs: **kwargs from Observer0D and _ObserverBase
+    """
+
+    cdef:
+        double _x_width, _y_width, _solid_angle, _collection_area
+        Primitive target
+        RectangleSampler3D _point_sampler
+        HemisphereCosineSampler _vector_sampler
+
+    def __init__(self, target, pipelines=None, x_width=None, y_width=None, parent=None, transform=None, name=None,
+                 render_engine=None, pixel_samples=None, samples_per_task=None, spectral_rays=None, spectral_bins=None,
+                 min_wavelength=None, max_wavelength=None, ray_extinction_prob=None, ray_extinction_min_depth=None,
+                 ray_max_depth=None, ray_importance_sampling=None, ray_important_path_weight=None):
+
+        pipelines = pipelines or [SpectralPipeline0D()]
+
+        super().__init__(pipelines, parent=parent, transform=transform, name=name, render_engine=render_engine,
+                         pixel_samples=pixel_samples, samples_per_task=samples_per_task, spectral_rays=spectral_rays,
+                         spectral_bins=spectral_bins, min_wavelength=min_wavelength, max_wavelength=max_wavelength,
+                         ray_extinction_prob=ray_extinction_prob, ray_extinction_min_depth=ray_extinction_min_depth,
+                         ray_max_depth=ray_max_depth, ray_importance_sampling=ray_importance_sampling,
+                         ray_important_path_weight=ray_important_path_weight)
+
+        self.target = target
+
+        self._x_width = 0.01
+        self._y_width = 0.01
+        self._vector_sampler = HemisphereCosineSampler()
+        self._solid_angle = 2 * PI
+
+        self.x_width = x_width or 0.01
+        self.y_width = y_width or 0.01
+
+    @property
+    def x_width(self):
+        """
+        The rectangular collection area's width along the x-axis in local coordinates.
+
+        :rtype: float
+        """
+        return self._x_width
+
+    @x_width.setter
+    def x_width(self, value):
+        if value <= 0:
+            raise RuntimeError("Pixel x-width must be greater than zero.")
+        self._x_width = value
+        self._point_sampler = RectangleSampler3D(width=self._x_width, height=self._y_width)
+        self._collection_area = self._x_width * self._y_width
+
+    @property
+    def y_width(self):
+        """
+        The rectangular collection area's width along the y-axis in local coordinates.
+
+        :rtype: float
+        """
+        return self._y_width
+
+    @y_width.setter
+    def y_width(self, value):
+        if value <= 0:
+            raise RuntimeError("Pixel y-width must be greater than zero.")
+        self._y_width = value
+        self._point_sampler = RectangleSampler3D(width=self._x_width, height=self._y_width)
+        self._collection_area = self._x_width * self._y_width
+
+    @property
+    def collection_area(self):
+        """
+        The pixel's collection area in m^2.
+
+        :rtype: float
+        """
+        return self._collection_area
+
+    @property
+    def solid_angle(self):
+        """
+        The pixel's solid angle in steradians str.
+
+        :rtype: float
+        """
+        return self._solid_angle
+
+    @property
+    def etendue(self):
+        """
+        The pixel's etendue measured in units of per area per solid angle (m^-2 str^-1).
+
+        :rtype: float
+        """
+        return self._pixel_etendue()
+
+    @cython.boundscheck(False)
+    @cython.wraparound(False)
+    cpdef list _generate_rays(self, Ray template, int ray_count):
+
+        cdef:
+            list rays, origins, directions
+            int n
+            double weight, radius
+            Point3D centre
+            BoundingSphere3D sphere
+
+        # test target primitive is in the same scenegraph as the observer
+        if self.target.root is not self.root:
+            raise ValueError("Target primitive is not in the same scenegraph as the TargetedPixel observer.")
+
+        # obtain bounding sphere and convert position to local coordinate space
+        sphere = self.target.bounding_sphere()
+        centre = sphere.centre.transform(self.to_local())
+        radius = sphere.radius
+
+
+        origins = self._point_sampler.samples(ray_count)
+
+        rays = []
+        for n in range(ray_count):
+
+            # for attempt in range(SAMPLE_ATTEMPTS):
+
+                direction = origin.vector_to(sphere.centre)
+                distance = direction.get_length()
+
+                # is point inside sphere?
+                if distance == 0 or distance < sphere.radius:
+                    # the point lies inside the sphere, sample random direction from full sphere
+                    return vector_sphere()
+
+                # calculate the angular radius and solid angle projection of the sphere
+                angular_radius = asin(sphere.radius / distance)
+
+                # sample a vector from a cone of half angle equal to the angular radius
+                sample = vector_cone_uniform(angular_radius * 180 / PI)
+
+                # rotate cone to lie along vector from observation point to sphere centre
+                direction = direction.normalise()
+                rotation = rotate_basis(direction, direction.orthogonal())
+                sample_direction =  sample.transform(rotation)
+
+                # check sample_direction is inside pixel hemisphere, if not, resample.
+                # if sample_direction.z > 0:
+                #     break
+
+            # else:
+                # sampling failed after 20 attempts
+                # sample from full cosine weighted hemisphere
+
+            # cosine weighted distribution
+            # projected area cosine is implicit in distribution
+            # weight = (1 / 2*pi) * (pi / cos(theta)) * cos(theta) = 0.5
+            rays.append((template.copy(origins[n], directions[n]), 0.5))
+
+        return rays
+
+    cpdef double _pixel_etendue(self):
+        return self._solid_angle * self._collection_area
+
+
