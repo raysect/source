0c621a8c1bd46864c362f102b24e25398a10af1b
Matthew Carr <MatthewNCarr@gmail.com>
2017-10-24 20:47:29 +0100

    TargetedPixel should support the quiet __init__() argument.

--- raysect/optical/observer/nonimaging/targeted_pixel.pyx
+++ raysect/optical/observer/nonimaging/targeted_pixel.pyx
@@ -76,7 +76,7 @@ cdef class TargetedPixel(Observer0D):
     def __init__(self, target, pipelines=None, x_width=None, y_width=None, parent=None, transform=None, name=None,
                  render_engine=None, pixel_samples=None, samples_per_task=None, spectral_rays=None, spectral_bins=None,
                  min_wavelength=None, max_wavelength=None, ray_extinction_prob=None, ray_extinction_min_depth=None,
-                 ray_max_depth=None, ray_importance_sampling=None, ray_important_path_weight=None):
+                 ray_max_depth=None, ray_importance_sampling=None, ray_important_path_weight=None, quiet=False):
 
         pipelines = pipelines or [SpectralPipeline0D()]
 
@@ -85,7 +85,7 @@ cdef class TargetedPixel(Observer0D):
                          spectral_bins=spectral_bins, min_wavelength=min_wavelength, max_wavelength=max_wavelength,
                          ray_extinction_prob=ray_extinction_prob, ray_extinction_min_depth=ray_extinction_min_depth,
                          ray_max_depth=ray_max_depth, ray_importance_sampling=ray_importance_sampling,
-                         ray_important_path_weight=ray_important_path_weight)
+                         ray_important_path_weight=ray_important_path_weight, quiet=quiet)
 
         self.target = target
 
