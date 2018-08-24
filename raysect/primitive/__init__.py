5a13488bc2eb0f9408535a873582adc8b0f1cc2e
CnlPepper <cnlpepper@gmail.com>
2018-08-24 20:30:32 +0100

    Fixed bug with mesh load not populating the vertex and triangles arrays, only the internal memory views. Changed version number to 0.5.3 as we intend to produce a bugfix release.

--- raysect/primitive/__init__.py
+++ raysect/primitive/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2015, Dr Alex Meakins, Raysect Project
+# Copyright (c) 2014-2018, Dr Alex Meakins, Raysect Project
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
