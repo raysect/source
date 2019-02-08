e5c1b1d095110ee6b2efed77936cd30db40b40ed
Dr Alex Meakins <CnlPepper@users.noreply.github.com>
2019-02-08 15:54:15 +0000

    Default to building with Cython.

--- dev/build.sh
+++ dev/build.sh
@@ -3,4 +3,4 @@
 CORES=`nproc --all`
 
 echo "Rebuilding Raysect extension modules (in place)..."
-python setup.py build_ext -j$CORES --inplace --use-cython $1 $2 $3 $4 $5
+python setup.py build_ext -j$CORES --inplace $1 $2 $3 $4 $5
