4c7b3b1ea01021c61e900a5475dc98172d636c2d
cnlpepper <cnlpepper@gmail.com>
2019-08-21 00:03:27 +0100

    Added material blending modifier. Closes #244.

---
+++ raysect/optical/material/modifiers/blend.pyx
@@ -0,0 +1,96 @@
+# cython: language_level=3
+
+# Copyright (c) 2014-2018, Dr Alex Meakins, Raysect Project
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#     1. Redistributions of source code must retain the above copyright notice,
+#        this list of conditions and the following disclaimer.
+#
+#     2. Redistributions in binary form must reproduce the above copyright
+#        notice, this list of conditions and the following disclaimer in the
+#        documentation and/or other materials provided with the distribution.
+#
+#     3. Neither the name of the Raysect Project nor the names of its
+#        contributors may be used to endorse or promote products derived from
+#        this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+from raysect.core.math.random import probability
+from raysect.optical cimport Point3D, Vector3D, Normal3D, new_normal3d, AffineMatrix3D, new_affinematrix3d, Primitive, World, Ray, Spectrum
+from raysect.optical.material cimport Material
+
+
+cdef class Blend(Material):
+    """
+    Blend combines the surface behaviours of two materials.
+
+    This modifier is used to blend together the surface behaviours of two
+    different materials. Which material handles the surface interaction for
+    an incoming ray is determined by a random choice, weighted by the ratio
+    argument. Low values of ratio bias the selection towards material 1, high
+    values to material 2. The volume behaviour is always handled by material 1.
+
+    Blend can be used to approximate finely sputtered surfaces consisting of a
+    mix of materials. For example it can be used to crudely approximate a gold
+    coated glass surface:
+
+        material = Blend(schott('N-BK7'), Gold(), 0.1)
+
+    :param m1: The first material.
+    :param m2: The second material.
+    :param ratio: A double value in the range (0, 1).
+    """
+
+    cdef:
+        Material m1, m2
+        double ratio
+
+    def __init__(self, Material m1 not None, Material m2 not None, double ratio):
+
+        super().__init__()
+
+        if ratio <= 0 or ratio >= 1.0:
+            raise ValueError("Ratio must be a floating point value in the range (0, 1).")
+
+        self.m1 = m1
+        self.m2 = m2
+        self.ratio = ratio
+
+    cpdef Spectrum evaluate_surface(self, World world, Ray ray, Primitive primitive, Point3D hit_point,
+                                    bint exiting, Point3D inside_point, Point3D outside_point,
+                                    Normal3D normal, AffineMatrix3D world_to_primitive, AffineMatrix3D primitive_to_world):
+
+        cdef:
+            Ray reflected
+            Vector3D s_incident, s_random
+            Normal3D s_normal
+            AffineMatrix3D surface_to_primitive
+            int attempt
+
+        if probability(self.ratio):
+            return self.m2.evaluate_surface(world, ray, primitive, hit_point, exiting, inside_point, outside_point,
+                                                  normal, world_to_primitive, primitive_to_world)
+        else:
+            return self.m1.evaluate_surface(world, ray, primitive, hit_point, exiting, inside_point, outside_point,
+                                                  normal, world_to_primitive, primitive_to_world)
+
+    cpdef Spectrum evaluate_volume(self, Spectrum spectrum, World world,
+                                   Ray ray, Primitive primitive,
+                                   Point3D start_point, Point3D end_point,
+                                   AffineMatrix3D to_local, AffineMatrix3D to_world):
+
+        return self.m1.evaluate_volume(spectrum, world, ray, primitive, start_point, end_point, to_local, to_world)
