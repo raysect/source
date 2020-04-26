83a92e8aef37c6ab13ecfeb8d16dac3ce3bdfaad
cnlpepper <cnlpepper@gmail.com>
2020-04-26 20:23:24 +0100

    Fixed copyright dates.

--- raysect/core/math/function/function3d/tests/test_autowrap.py
+++ raysect/core/math/function/function3d/tests/test_autowrap.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2014-2019, Dr Alex Meakins, Raysect Project
+# Copyright (c) 2014-2020, Dr Alex Meakins, Raysect Project
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
