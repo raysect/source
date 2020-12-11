e670acf70ce65b6c814615cd004bd2f27755a7f6
Koyo MUNECHIKA <munechika.koyo@torus.nr.titech.ac.jp>
2020-12-11 10:26:32 +0900

    documentation for tetrahedra mesh

--- demos/materials/tetrahedra_mesh_emission.py
+++ demos/materials/tetrahedra_mesh_emission.py
@@ -28,7 +28,7 @@ class UnityEmitter(InhomogeneousVolumeEmitter):
 
 
 # Pig tetrahedra mesh emitter
-mesh = Tetmesh(os.path.join(BASE_PATH, "../resources/pig_tet.mesh"))
+mesh = Tetmesh(os.path.join(BASE_PATH, "../resources/stanford_bunny.mesh"))
 tetra = Discrete3DMesh(mesh.vertices, mesh.tets, np.ones((mesh.num_tets)), False, 0)
 
 # scene
@@ -36,20 +36,21 @@ world = World()
 lower = Point3D(mesh.bbox[0, 0], mesh.bbox[0, 1], mesh.bbox[0, 2])
 upper = Point3D(mesh.bbox[1, 0], mesh.bbox[1, 1], mesh.bbox[1, 2])
 emitter = Box(lower, upper, material=UnityEmitter(tetra), parent=world)
-floor = Box(Point3D(-100, -0.3, -100), Point3D(100, -0.2, 100), world, material=RoughTitanium(0.1))
+floor = Box(Point3D(-100, -0.1, -100), Point3D(100, -0.01, 100), world, material=RoughTitanium(0.1))
 
 # camera
-camera_pos = Point3D(-0.6, 0.25, -1.1)
+camera_pos = Point3D(-0.015, 0.204, 0.25)
 rgb_pipeline = RGBPipeline2D(display_update_time=5)
 sampler = RGBAdaptiveSampler2D(rgb_pipeline, min_samples=100, fraction=0.2)
-camera = PinholeCamera((512, 512), parent=world, transform=translate(camera_pos.x, camera_pos.y, camera_pos.z) * rotate_basis(camera_pos.vector_to(Point3D(0, 0, 0)), Vector3D(0, 1, 0)), pipelines=[rgb_pipeline], frame_sampler=sampler)
+camera = PinholeCamera((512, 512), parent=world, transform=translate(camera_pos.x, camera_pos.y, camera_pos.z) * rotate_basis(camera_pos.vector_to(Point3D(0, 0.06, 0)), Vector3D(0, 1, 0)),
+                       pipelines=[rgb_pipeline], frame_sampler=sampler)
 camera.fov = 50
 camera.spectral_bins = 1
 camera.spectral_rays = 1
 camera.pixel_samples = 200
 
 # integration resolution
-emitter.material.integrator.step = 0.005
+emitter.material.integrator.step = 0.0001
 
 # start ray tracing
 os.nice(15)
