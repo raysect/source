b6210e4ac7e14faaffe3df8a73e2a3c2d7b2cce6
Dr Alex Meakins <alex.meakins@luffy.ai>
2021-03-26 16:57:15 +0000

    Added MuellerMatrix.

--- raysect/optical/polarised/mueller.pyx
+++ raysect/optical/polarised/mueller.pyx
@@ -1,5 +1,9 @@
 from raysect.core.math._mat4 cimport _Mat4
+from raysect.core.math cimport Vector3D
+from libc.math cimport sqrt, sin, cos
 
+DEF RAD2DEG = 57.29577951308232000  # 180 / pi
+DEF DEG2RAD = 0.017453292519943295  # pi / 180
 
 # todo: write docstring
 cdef class MuellerMatrix(_Mat4):
@@ -75,3 +79,60 @@ cdef class MuellerMatrix(_Mat4):
             self.m[3][0] * m.m[0][2] + self.m[3][1] * m.m[1][2] + self.m[3][2] * m.m[2][2] + self.m[3][3] * m.m[3][2],
             self.m[3][0] * m.m[0][3] + self.m[3][1] * m.m[1][3] + self.m[3][2] * m.m[2][3] + self.m[3][3] * m.m[3][3]
         )
+
+
+cpdef MuellerMatrix depolariser():
+    return new_muellermatrix(
+        1, 0, 0, 0,
+        0, 0, 0, 0,
+        0, 0, 0, 0,
+        0 ,0 ,0 ,0
+    )
+
+
+cpdef MuellerMatrix diattenuator(tx, ty):
+    """
+    Mueller matrix for a linear diattenuator (polariser).
+    
+    Calculates the diattenuation. The x-axis transmission corresponds to the
+    horizontal polarisation of the stokes vector, with the y-axis the vertical
+    polarisation. If either tx or ty are zero, the diattenuator acts as a
+    linear polariser. If tx and ty are both non-zero the diattenuator will
+    introduce a circular polarisation component.
+    
+    :param double tx: Transmission in the x axis.
+    :param double ty: Transmission in the y axis.  
+    """
+
+    cdef double a = 0.5 * (tx + ty)
+    cdef double b = 0.5 * (tx - ty)
+    cdef double c = 0.5 * sqrt(tx * ty)
+
+    return new_muellermatrix(
+        a, b, 0, 0,
+        b, a, 0, 0,
+        0, 0, c, 0,
+        0 ,0 ,0 ,c
+    )
+
+
+# retarder
+
+
+cpdef MuellerMatrix rotate_angle(double angle):
+    """
+    Rotates a Stoke's vector to from a source frame to a target frame by angle.
+    
+    :param double angle: Angle in degrees between the source and target frames.
+    """
+
+    angle = RAD2DEG * angle
+    cdef double s = sin(2*angle)
+    cdef double c = cos(2*angle)
+
+    return new_muellermatrix(
+        1, 0, 0, 0,
+        0, c, s, 0,
+        0, -s, c, 0,
+        0 ,0 ,0 ,1
+    )
