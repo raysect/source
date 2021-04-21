ab98f60e8a528eaaeba71ca9cd977e93a3b5a7bc
Dr Alex Meakins <alex.meakins@luffy.ai>
2021-04-21 18:44:00 +0100

    Transmission experiments.

--- fresnel.py
+++ fresnel.py
@@ -30,7 +30,7 @@ def fresnel_rp(n1, n2, angle):
 
     ci = math.cos(angle)
     ct = math.cos(transmission_angle(n1, n2, angle))
-    return -(n2 * ci - n1 * ct) / (n2 * ci + n1 * ct)
+    return (n1*ct - n2*ci) / (n2*ci + n1*ct)
 
 
 def fresnel_rs(n1, n2, angle):
@@ -44,7 +44,7 @@ def fresnel_rs(n1, n2, angle):
 
     ci = math.cos(angle)
     ct = math.cos(transmission_angle(n1, n2, angle))
-    return (n1 * ci - n2 * ct) / (n1 * ci + n2 * ct)
+    return (n1*ci - n2*ct) / (n1*ci + n2*ct)
 
 
 def fresnel_tp(n1, n2, angle):
@@ -52,62 +52,85 @@ def fresnel_tp(n1, n2, angle):
     Fresnel transmission coefficient for parallel E-field.
     """
 
-    if angle == 0:
-        # todo: take limit of original eqn?
-        return 1.0 + fresnel_rp(n1, n2, angle)
-
     if n1 > n2 and angle > total_internal_reflection_angle(n1, n2):
         return 0.0
 
     t = transmission_angle(n1, n2, angle)
-    return fresnel_ts(n1, n2, angle) / math.cos(angle - t)
+    if t == 0 or angle == 0:
+        return 1.0
+    # return 2 * n1 * math.cos(angle) / (n2 * math.cos(angle) + n1 * math.cos(t))
+    return math.sqrt(math.sin(2*angle) * math.sin(2*t) / (math.sin(angle+t) * math.cos(angle-t))**2)
 
 
 def fresnel_ts(n1, n2, angle):
     """
     Fresnel transmission coefficient for perpendicular E-field.
     """
-    if angle == 0:
-        # todo: take limit of original eqn?
-        return 1 + fresnel_rs(n1, n2, angle)
+    # if angle == 0:
+    #     # todo: take limit of original eqn?
+    #     #return 1 + fresnel_rs(n1, n2, angle)
+    #     return 1 + (n1 - n2) / (n1 + n2)
 
     if n1 > n2 and angle > total_internal_reflection_angle(n1, n2):
         return 0.0
 
     t = transmission_angle(n1, n2, angle)
-    return 2 * (math.cos(angle) * math.sin(t)) / (math.sin(angle + t))
+    if t == 0 or angle == 0:
+        return 1.0
+    # return 2 * n1 * math.cos(angle) / (n1 * math.cos(angle) + n2 * math.cos(t))
+    return math.sqrt(math.cos(angle - t)**2 * math.sin(2*angle) * math.sin(2*t) / (math.sin(angle+t) * math.cos(angle-t))**2)
 
 
+def plot_fresnel(n1, n2):
+    # incident_angle = math.radians(25)
+    # transmitted_angle = transmission_angle(n1, n2, incident_angle)
+    #
+    # print(f'incident angle: {math.degrees(incident_angle)}')
+    # print(f'transmission angle: {math.degrees(transmitted_angle)}')
+    # print(f'Brewster\'s angle: {math.degrees(brewster_angle(n1, n2))}')
+    # print(f'TIR angle: {math.degrees(total_internal_reflection_angle(n1, n2))}')
 
-n1 = 1.0
-n2 = 1.5
-incident_angle = math.radians(56.31)
-# transmitted_angle = transmission_angle(n1, n2, incident_angle)
+    n = 1000
+    a = np.linspace(0, 90, n)
+    rp = np.zeros(n)
+    rs = np.zeros(n)
+    tp = np.zeros(n)
+    ts = np.zeros(n)
+    Rp = np.zeros(n)
+    Rs = np.zeros(n)
+    Tp = np.zeros(n)
+    Ts = np.zeros(n)
+    for i in range(n):
+        rp[i] = fresnel_rp(n1, n2, math.radians(a[i]))
+        rs[i] = fresnel_rs(n1, n2, math.radians(a[i]))
+        tp[i] = fresnel_tp(n1, n2, math.radians(a[i]))
+        ts[i] = fresnel_ts(n1, n2, math.radians(a[i]))
+        Rp[i] = rp[i]**2
+        Rs[i] = rs[i]**2
+        Tp[i] = tp[i]**2
+        Ts[i] = ts[i]**2
 
-print(f'incident angle: {math.degrees(incident_angle)}')
-# print(f'transmission angle: {math.degrees(transmitted_angle)}')
-print(f'Brewster\'s angle: {math.degrees(brewster_angle(n1, n2))}')
-print(f'TIR angle: {math.degrees(total_internal_reflection_angle(n1, n2))}')
+    plt.figure()
+    plt.title(f'Coefficients n1={n1}, n2={n2}')
+    plt.grid()
+    plt.plot(a, rp)
+    plt.plot(a, rs)
+    plt.plot(a, tp)
+    plt.plot(a, ts)
+    plt.legend(['rp', 'rs', 'tp', 'ts'])
 
-n = 100
-a = np.linspace(0, 90, n)
-rp = np.zeros(n)
-rs = np.zeros(n)
-tp = np.zeros(n)
-ts = np.zeros(n)
-for i in range(n):
-    rp[i] = fresnel_rp(n1, n2, math.radians(a[i]))
-    rs[i] = fresnel_rs(n1, n2, math.radians(a[i]))
-    tp[i] = fresnel_tp(n1, n2, math.radians(a[i]))
-    ts[i] = fresnel_ts(n1, n2, math.radians(a[i]))
+    plt.figure()
+    plt.title(f'Reflection/transmission n1={n1}, n2={n2}')
+    plt.grid()
+    plt.plot(a, Rp)
+    plt.plot(a, Rs)
+    plt.plot(a, 0.5*(Rp + Rs))
+    plt.plot(a, Tp)
+    plt.plot(a, Ts)
+    plt.plot(a, 0.5*(Tp + Ts))
+    plt.legend(['Rp', 'Rs', 'R', 'Tp', 'Ts', 'T'])
 
-plt.figure()
-plt.grid()
-plt.plot(a, rp)
-plt.plot(a, rs)
-plt.plot(a, tp)
-plt.plot(a, ts)
-plt.legend(['rp', 'rs', 'tp', 'ts'])
+
+plot_fresnel(1.0, 1.5)
+plot_fresnel(1.5, 1.0)
 plt.show()
-
-
