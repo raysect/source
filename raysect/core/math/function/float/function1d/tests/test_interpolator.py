db071fe569a703148e86d055fd7a079bd283b78e
simon <simon.orchard1@btinternet.com>
2021-06-03 17:54:14 +0100

    Cubic generation and testing sampled points

--- raysect/core/math/function/float/function1d/tests/test_interpolator.py
+++ raysect/core/math/function/float/function1d/tests/test_interpolator.py
@@ -34,7 +34,7 @@ including interaction with Extrapolator1DLinear and Extrapolator1DNearest.
 """
 import unittest
 import numpy as np
-from raysect.core.math.function.float.function1d.interpolate import Interpolate1D
+from raysect.core.math.function.float.function1d.interpolate import Interpolate1D, id_to_extrapolator
 
 
 X_LOWER = 0.0
@@ -44,6 +44,10 @@ X_EXTRAP_DELTA_MIN = 0.04
 
 NB_X = 10
 NB_XSAMPLES = 30
+
+EXTRAPOLATION_RANGE = 0.06
+
+PRECISION = 12
 
 
 class TestInterpolators1D(unittest.TestCase):
@@ -56,9 +60,10 @@ class TestInterpolators1D(unittest.TestCase):
         self.x = np.linspace(X_LOWER, X_UPPER, NB_X)
 
         #: data array from a function sampled on self.x. dtype should be np.float64
+        # self.data: np.array = np.sin(self.x)
         self.data: np.array = np.array(
-            [0., 0.11088263, 0.22039774, 0.3271947 , 0.42995636, 0.52741539, 0.6183698 , 0.70169788, 0.77637192,
-             0.84147098]
+            [0., 0.11088262851 , 0.220397743456, 0.327194696796, 0.429956363528, 0.527415385772, 0.61836980307,
+             0.701697876147, 0.776371921301, 0.841470984808]#, dtype=np.float64
         )
 
         #: precalculated result of sampling self.data on self.xsamples
@@ -67,22 +72,25 @@ class TestInterpolators1D(unittest.TestCase):
 
         #: precalculated result of the function used to calculate self.data on self.xsamples
         self.precalc_function = np.array(
-            [0., 0.03447593, 0.06891086, 0.10326387, 0.13749409, 0.17156085, 0.20542363, 0.23904218, 0.27237651,
-             0.30538701, 0.33803442, 0.37027992, 0.40208519, 0.43341239, 0.4642243, 0.49448427, 0.52415632, 0.55320518,
-             0.58159632, 0.60929597, 0.6362712, 0.66248994, 0.68792102, 0.7125342, 0.73630021, 0.75919081, 0.78117877,
-             0.80223796, 0.82234333, 0.84147098]
+            [0., 0.034475925345, 0.068910860786, 0.103263865154, 0.13749409469, 0.171560851609, 0.205423632484,
+             0.239042176405, 0.272376512846, 0.305387009186, 0.338034417834, 0.370279922885, 0.402085186279,
+             0.433412393376, 0.464224297918, 0.494484266311, 0.524156321185, 0.553205184161, 0.581596317803,
+             0.609295966676, 0.636271197481, 0.662489938212, 0.687921016284, 0.7125341956, 0.736300212497,
+             0.759190810539, 0.781178774115, 0.802237960792, 0.822343332402, 0.841470984808], dtype=np.float64
         )
 
         #: x values on which self.precalc_interpolation was samples on
         self.xsamples = np.linspace(X_LOWER, X_UPPER, NB_XSAMPLES)
 
-        #: array holding precalculated linear extrapolation data
-        #   to be set in interpolator specific setup_ method
-        self.precalc_extrapolation_linear: np.array = None
+        #: array holding precalculated nearest neighbour extrapolation data
+        self.precalc_extrapolation_nearest: np.array = np. array(
+            [0., 0., 0.841470984808, 0.841470984808], dtype=np.float64
+        )
 
-        #: array holding precalculated cubic extrapolation data
-        #   to be set in interpolator specific setup_ method
-        self.precalc_extrapolation_cubic: np.array = None
+        #: array holding precalculated linear extrapolation data
+        self.precalc_extrapolation_linear: np.array = np.array(
+            [-0.079835492527, -0.039917746264,  0.864906647671,  0.888342310533], dtype=np.float64
+        )
 
         #: x values on which self.precalc_extrapolation_ arrays were sampled on
         self.xsamples_extrap = np.array(
@@ -94,6 +102,9 @@ class TestInterpolators1D(unittest.TestCase):
             ],
             dtype=np.float64,
         )
+
+        #: set precalculated expected extrapolation results  Set in setup_ method
+        self.precalc_extrapolation = None
 
         #: the interpolator object that is being tested. Set in setup_ method
         self.interpolator: Interpolate1D = None
@@ -116,15 +127,11 @@ class TestInterpolators1D(unittest.TestCase):
         self.precalc_interpolation = None
 
         # set precalculated expected extrapolation results
-        # this is the result of nearest neighbour extrapolation on self.xsamples_extrap
-        self.precalc_extrapolation_nearest = None
-
-        # set precalculated expected extrapolation results
-        # this is the result of linear extrapolation on self.xsamples_extrap
-        self.precalc_extrapolation_linear = None
+        # this is the result of the type of extrapolation on self.xsamples_extrap
+        self.setup_extrpolation_type(extrapolator_type)
 
         # set interpolator
-        self.interpolator = Interpolate1D(self.x, self.data, "linear", extrapolator_type, extrapolation_range)
+        self.interpolator = Interpolate1D(self.x, self.data, 'linear', extrapolator_type, extrapolation_range)
 
     def setup_cubic(self, extrapolator_type: str, extrapolation_range: float):
         """
@@ -142,40 +149,74 @@ class TestInterpolators1D(unittest.TestCase):
         # set precalculated expected interpolation results
         # this is the result of sampling self.data on self.xsamples
         self.precalc_interpolation = np.array(
-            [0., 0.03445727, 0.06892362, 0.10327643, 0.13747237, 0.17156602, 0.20544274, 0.23902114, 0.27237365,
-             0.30540749, 0.33801842, 0.37026979, 0.40210303, 0.43340598, 0.46420873, 0.49449677, 0.52416232, 0.55318685,
-             0.58160215, 0.609309, 0.63625324, 0.66248906, 0.68793574, 0.7125198, 0.73629368, 0.75920346, 0.78114771,
-             0.80176427, 0.82158583, 0.84147098]
+            [0., 0.034457267669, 0.068923618826, 0.103276426379, 0.137472367276, 0.171566022088, 0.205442737431,
+             0.239021136276, 0.27237364671, 0.305407493229, 0.338018423689, 0.37026978657, 0.40210303144, 0.43340598175,
+             0.464208732427, 0.494496772755, 0.524162321048, 0.553186854304, 0.581602151176, 0.609309003405,
+             0.636253243661, 0.662489056087, 0.687935735681, 0.712519804849, 0.736293678079, 0.759203461591,
+             0.78114771311, 0.801764272516, 0.821585828526, 0.841470984808]#, dtype=np.float64
         )
-
-        # set precalculated expected extrapolation results
-        # this is the result of nearest neighbour extrapolation on self.xsamples_extrap
-        self.precalc_extrapolation_linear = None
-
-        # set precalculated expected extrapolation results
-        # this is the result of linear extrapolation on self.xsamples_extrap
-        self.precalc_extrapolation_cubic = None
-
+        self.setup_extrpolation_type(extrapolator_type)
         # set interpolator
-        self.interpolator = Interpolate1D(self.x, self.data, "linear", extrapolator_type, extrapolation_range)
+        self.interpolator = Interpolate1D(self.x, self.data, 'cubic', extrapolator_type, extrapolation_range)
+
+    def setup_extrpolation_type(self, extrapolator_type: str):
+        if extrapolator_type == 'linear':
+            self.precalc_extrapolation = np.copy(self.precalc_extrapolation_linear)
+        elif extrapolator_type == 'nearest':
+            self.precalc_extrapolation = np.copy(self.precalc_extrapolation_nearest)
+        elif extrapolator_type == 'none':
+            self.precalc_extrapolation = None
+        else:
+            raise ValueError(
+                f'Extrapolation type {extrapolator_type} not found or no test. options are {id_to_extrapolator.keys()}'
+            )
 
     def test_linear_interpolation(self):
-        self.setup_linear("none", 0.0)
+        self.setup_linear('none', 0.0)
 
     def test_linear_interpolation_extrapolators(self):
-        self.setup_linear("nearest", 0.0)
-        # test linear interpolation with 'nearest' extrapolator here
+        self.setup_linear('nearest', EXTRAPOLATION_RANGE)
 
-        self.setup_linear("linear", 0.0)
+        # test linear interpolation with 'nearest' extrapolator here
+        self.run_general_extrapolation_tests()
+
+        self.setup_linear('linear', EXTRAPOLATION_RANGE)
         # test linear interpolation with 'linear' extrapolator here
+        self.run_general_extrapolation_tests()
 
     def test_cubic_interpolation_extrapolators(self):
         """
         Testing against scipy.interpolate.CubicHermiteSpline with the same gradient calculations generated using scipy
         version 1.6.3
         """
-        self.setup_cubic("nearest", 0.0)
+        self.setup_cubic('nearest', EXTRAPOLATION_RANGE)
         # test cubic interpolation with 'nearest' extrapolator here
+        self.run_general_extrapolation_tests()
+        self.run_general_interpolation_tests()
 
-        self.setup_cubic("linear", 0.0)
+        self.setup_cubic('linear', EXTRAPOLATION_RANGE)
         # test cubic interpolation with 'linear' extrapolator here
+        self.run_general_extrapolation_tests()
+        self.run_general_interpolation_tests()
+
+    def run_general_extrapolation_tests(self):
+        # Test extrapolator out of range, there should be an error raised
+        self.assertRaises(ValueError, self.interpolator, self.xsamples_extrap[0])
+        self.assertRaises(ValueError, self.interpolator, self.xsamples_extrap[-1])
+
+        # Test extrapolation inside extrapolation range matches the predefined values
+        self.assertAlmostEqual(
+            self.interpolator(self.xsamples_extrap[1]), self.precalc_extrapolation[1], places=PRECISION - 1
+        )
+
+    def run_general_interpolation_tests(self):
+
+        # Test continuity of smoothness
+
+        # Test interpolation against xsample
+        for i in range(len(self.xsamples)):
+            self.assertAlmostEqual(
+                self.interpolator(self.xsamples[i]), self.precalc_interpolation[i], places=PRECISION - 1
+            )
+
+
