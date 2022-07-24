ef73bd6e7f17e1a016ccd9c4ea88f14053b5fe24
Koyo Munechika <munechika.koyo@torus.nr.ttitech.ac.jp>
2022-07-24 19:03:24 +0900

    [fix] error about circular reference

--- raysect/primitive/mesh/obj.py
+++ raysect/primitive/mesh/obj.py
@@ -29,7 +29,7 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-from raysect.core import Normal3D
+from raysect.core.math import Normal3D
 from raysect.primitive.mesh import Mesh
 
 
