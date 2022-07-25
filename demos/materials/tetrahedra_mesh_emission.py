b96b24fb0c7f78673f6826c2d4c74365b92cb11b
Koyo Munechika <munechika.koyo@torus.nr.ttitech.ac.jp>
2022-07-25 13:40:16 +0900

    [update] demo script for tetrahedra mesh emission

--- demos/materials/tetrahedra_mesh_emission.py
+++ demos/materials/tetrahedra_mesh_emission.py
@@ -42,8 +42,19 @@ floor = Box(Point3D(-100, -0.1, -100), Point3D(100, -0.01, 100), world, material
 camera_pos = Point3D(-0.015, 0.204, 0.25)
 rgb_pipeline = RGBPipeline2D(display_update_time=5)
 sampler = RGBAdaptiveSampler2D(rgb_pipeline, min_samples=100, fraction=0.2)
-camera = PinholeCamera((512, 512), parent=world, transform=translate(camera_pos.x, camera_pos.y, camera_pos.z) * rotate_basis(camera_pos.vector_to(Point3D(0, 0.06, 0)), Vector3D(0, 1, 0)),
-                       pipelines=[rgb_pipeline], frame_sampler=sampler)
+camera = PinholeCamera(
+    (512, 512),
+    parent=world,
+    pipelines=[rgb_pipeline],
+    frame_sampler=sampler,
+    transform=(
+        translate(camera_pos.x, camera_pos.y, camera_pos.z)
+        * rotate_basis(
+            camera_pos.vector_to(Point3D(0, 0.06, 0)),
+            Vector3D(0, 1, 0)
+        )
+    )
+)
 camera.fov = 50
 camera.spectral_bins = 1
 camera.spectral_rays = 1
@@ -56,7 +67,7 @@ emitter.material.integrator.step = 0.0001
 os.nice(15)
 plt.ion()
 timestamp = time.strftime("%Y-%m-%d_%H-%M-%S")
-for p in range(1, 1000):
+for p in range(1, 100):
     print("Rendering pass {}...".format(p))
     camera.observe()
     rgb_pipeline.save("demo_volume_{}_pass_{}.png".format(timestamp, p))
