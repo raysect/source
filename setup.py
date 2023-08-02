2f7b3f8dd43fba4fbfd56e968931f656136ae32d
Koyo Munechika <munechika.koyo@torus.nr.ttitech.ac.jp>
2023-08-02 16:40:41 +0900

    🎨 format too long lines by indenting them

--- setup.py
+++ setup.py
@@ -60,13 +60,27 @@ if use_cython:
                 if path.splitext(file)[1] == ".pyx":
                     pyx_file = path.relpath(path.join(root, file), setup_path)
                     module = path.splitext(pyx_file)[0].replace("/", ".")
-                    extensions.append(Extension(module, [pyx_file], include_dirs=compilation_includes, extra_compile_args=compilation_args, define_macros=macros),)
+                    extensions.append(
+                        Extension(
+                            module,
+                            [pyx_file],
+                            include_dirs=compilation_includes,
+                            extra_compile_args=compilation_args,
+                            define_macros=macros,
+                        ),
+                    )
 
     if profile:
         cython_directives["profile"] = True
 
     # generate .c files from .pyx
-    extensions = cythonize(extensions, nthreads=multiprocessing.cpu_count(), force=force, compiler_directives=cython_directives, annotate=annotate)
+    extensions = cythonize(
+        extensions,
+        nthreads=multiprocessing.cpu_count(),
+        force=force,
+        compiler_directives=cython_directives,
+        annotate=annotate
+    )
 
 else:
 
@@ -78,7 +92,15 @@ else:
                 if path.splitext(file)[1] == ".c":
                     c_file = path.relpath(path.join(root, file), setup_path)
                     module = path.splitext(c_file)[0].replace("/", ".")
-                    extensions.append(Extension(module, [c_file], include_dirs=compilation_includes, extra_compile_args=compilation_args, define_macros=macros),)
+                    extensions.append(
+                        Extension(
+                            module,
+                            [c_file],
+                            include_dirs=compilation_includes,
+                            extra_compile_args=compilation_args,
+                            define_macros=macros,
+                        ),
+                    )
 
 # parse the package version number
 with open(path.join(path.dirname(__file__), 'raysect/VERSION')) as version_file:
