efc79fff8a49b68545b1423e35bd1a533c81309c
Dr Alex Meakins <alex.meakins@luffy.ai>
2025-07-19 17:18:47 +0100

    Updated build.sh and clean.sh scripts. Added abi.py to obtain the meson-python build folder. Added install_editable.sh to simplify installing the package in editable mode with verbose build information enabled for meson-python. Moved cython configuration to the root meson.build so it is shared across the project.

--- dev/build.sh
+++ dev/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+set -e  # exit if an error occurs
 
-CORES=`nproc --all`
+BUILD_PATH="build/`dev/abi.py`"
 
-echo "Rebuilding Raysect extension modules (in place)..."
-python setup.py build_ext -j$CORES --inplace $1 $2 $3 $4 $5
+echo Rebuilding $BUILD_PATH...
+meson compile -C $BUILD_PATH
